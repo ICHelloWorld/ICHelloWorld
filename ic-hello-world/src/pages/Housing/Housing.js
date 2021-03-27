8101627bbaf41df12d69db64580d835ddab906a9
Madi Baig <madibaig18@yahoo.com>
2021-03-27 22:10:35 +0000

    Housing heading

---
+++ ic-hello-world/src/pages/Housing/Housing.js
@@ -0,0 +1,9 @@
+export default function Housing() {
+    return (
+        <div>
+            <div className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8"}>
+                <h1>Housing</h1>
+            </div>
+        </div>
+    )
+}
