4c70e00c3ba41fe0c2babcf0430d0ae8c35fae04
shashwatkansal <sk4319@ic.ac.uk>
2021-03-27 16:58:51 +0000

    Add Basic Profile heading

--- ic-hello-world/src/pages/Profile/Profile.js
+++ ic-hello-world/src/pages/Profile/Profile.js
@@ -1,7 +1,13 @@
 export default function ProfilePage() {
   return (
-    <div className={"bg-gray-100"}>
-      <p>Profile Page</p>
+    <div className={"bg-gray-100 "}>
+      <div className={"text-center text-xl font-semibold py-8"}>
+        <h1 className={"font-bold text-4xl text-blue-600"}>Profile</h1>
+        <p>Fill out your profile so that you can connect with others!</p>
+      </div>
+      <div className={"bg-white rounded-xl shadow-xl mx-auto"}>
+
+      </div>
     </div>
   )
 }
