d3b962c1386f4e05d6388762dd2a0d7637ee4aca
shashwatkansal <sk4319@ic.ac.uk>
2021-03-28 10:55:50 +0100

    Add quick bug fixes for Profiel

--- ic-hello-world/src/pages/Profile/Profile.js
+++ ic-hello-world/src/pages/Profile/Profile.js
@@ -1,12 +1,5 @@
-import {notification} from "antd";
+import {Link} from "react-router-dom";
 import {app} from "../../firebaseConfig"
-
-const openNotificationWithIcon = (type, desc = "Done!") => {
-  notification[type]({
-    message: "Success",
-    description: desc,
-  });
-};
 
 export default function ProfilePage() {
 
@@ -20,123 +13,6 @@ export default function ProfilePage() {
     )
   }
 
-  const ProfileInfo = () => {
-    return (
-      <div className={""}>
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="md:col-span-1">
-            <div className="px-4 sm:px-0">
-              <h3
-                className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-              <p className="mt-1 text-sm text-gray-600">
-                This information will be displayed publicly so be careful what
-                you share.
-              </p>
-            </div>
-          </div>
-          <div className="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-              <div className="shadow sm:rounded-md sm:overflow-hidden">
-                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="company_website"
-                             className="block text-sm font-medium text-gray-700">
-                        Website
-                      </label>
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                  <span
-                    className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                    http://
-                  </span>
-                        <input type="text" name="company_website"
-                               id="company_website"
-                               className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                               placeholder="www.example.com"/>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label htmlFor="about"
-                           className="block text-sm font-medium text-gray-700">
-                      About
-                    </label>
-                    <div className="mt-1">
-                      <textarea id="about" name="about" rows="3"
-                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                                placeholder="you@example.com"/>
-                    </div>
-                    <p className="mt-2 text-sm text-gray-500">
-                      Brief description for your profile. URLs are hyperlinked.
-                    </p>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                      Photo
-                    </label>
-                    <div className="mt-1 flex items-center">
-                <span
-                  className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                  <svg className="h-full w-full text-gray-300"
-                       fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                      d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"/>
-                  </svg>
-                </span>
-                      <button type="button"
-                              className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Change
-                      </button>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                      Cover photo
-                    </label>
-                    <div
-                      className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg className="mx-auto h-12 w-12 text-gray-400"
-                             stroke="currentColor" fill="none"
-                             viewBox="0 0 48 48" aria-hidden="true">
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round"/>
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label htmlFor="file-upload"
-                                 className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload"
-                                   type="file" className="sr-only"/>
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs text-gray-500">
-                          PNG, JPG, GIF up to 10MB
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button type="submit"
-                          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   function handleProfileForm(event) {
     event.preventDefault();
     let {
@@ -145,23 +21,23 @@ export default function ProfilePage() {
     } = event.target.elements;
     console.log(event.target.elements)
     let data = {
-      firstname: first_name.value,
-      lastname: last_name.value,
-      email: email_address.value,
-      description: description.value,
-      languages: languages.value,
-      pricerange: pricerange.value,
-      shortcode: shortcode.value,
-      year: year.value,
-      department: department.value,
-      howmanypeople: howmanypeople.value,
+      firstname: first_name.value || "",
+      lastname: last_name.value || "",
+      email: email_address.value || "",
+      description: description.value || "",
+      languages: languages.value || "",
+      pricerange: pricerange.value || "",
+      shortcode: shortcode.value || "",
+      year: year.value || "",
+      department: department.value || "",
+      howmanypeople: howmanypeople.value || "",
     }
     console.log(data)
     const db = app.firestore()
-    db.collections("profile").add(data)
+    db.collection("profile").add(data)
       .then((result) => {
         // Read result of the Cloud Function.
-        openNotificationWithIcon("success")
+        alert("success")
       })
       .catch((error) => {
         // Getting the Error details.
@@ -169,7 +45,7 @@ export default function ProfilePage() {
         var message = error.message;
         var details = error.details;
         // ...
-        openNotificationWithIcon("success", message)
+        alert("success", message)
       })
 
   }
@@ -348,6 +224,14 @@ export default function ProfilePage() {
 
   return (
     <div className={"bg-gray-100 px-8 py-8"}>
+      <div className="heading">
+        <ul>
+          <li><Link to="/"> Home</Link></li>
+          <li><Link to="/profile"> Profile</Link></li>
+          <li><Link to="/search"> Search</Link></li>
+          <li><Link to="/feed"> Feed</Link></li>
+        </ul>
+      </div>
       <div className={"text-center text-xl font-semibold py-8"}>
         <h1 className={"font-bold text-4xl text-blue-600"}>Profile</h1>
         <p>Fill out your profile so that you can connect with others!</p>
@@ -355,7 +239,6 @@ export default function ProfilePage() {
       <PageBreak/>
       <PersonalInfo/>
       <PageBreak/>
-
     </div>
 
   )
--- ic-hello-world/src/pages/Search/Search.js
+++ ic-hello-world/src/pages/Search/Search.js
@@ -1,44 +1,49 @@
 import {Link} from 'react-router-dom';
+import {db} from '../../firebaseConfig'
 
 export default function Search() {
-    return (
-        /*
-        <div className={"bg-gray-100 "}>
-      <div className={"text-center text-xl font-semibold py-8"}>
-        <h1 className={"font-bold text-4xl text-blue-600"}>Profile</h1>
-        <p>Fill out your profile so that you can connect with others!</p>
+  const GetAllData = async () => {
+    const citiesRef = db.collection('profile');
+    const snapshot = await citiesRef.get();
+    snapshot.forEach(doc => {
+      return (
+        <div className={"bg-white rounded-xl shadow-xl p-4 m-2"}>
+          <h1 className={"text-2xl text-black font-semibold"}>{doc}</h1>
+        </div>
+      )
+    });
+  }
+  return (
+    <div>
+      <div className="heading">
+        <ul>
+          <li><Link to="/"> Home</Link></li>
+          <li><Link to="/profile"> Profile</Link></li>
+          <li><Link to="/search"> Search</Link></li>
+          <li><Link to="/feed"> Feed</Link></li>
+        </ul>
       </div>
-      <div className={"bg-white rounded-xl shadow-xl mx-auto"}>
+      <div
+        className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8"}>
+        <h1>Search Page</h1>
+      </div>
+      <label>Price Range (£ per month): </label>
+      <input placeholder="e.g. 100-400"></input>
+      <label>Department: </label>
+      <input placeholder="e.g. Computing"></input>
+      <label>Year: </label>
+      <input placeholder="e.g. 2"></input>
+      <label>Night Owl</label>
+      <input type="checkbox"></input>
+      <label>Morning Scrub</label>
+      <input type="checkbox"></input>
+      <div className={"mt-4 p-8"}>
+        {/*{GetAllData()}*/}
       </div>
     </div>
-         */
-
-        <div>
-        <div class="heading">
-          <ul>
-            <li><Link to="/"> Home</Link></li>
-            <li><Link to="/profile"> Profile</Link></li>
-            <li><Link to="/search"> Search</Link></li>
-            <li><Link to="/feed"> Feed</Link></li>
-          </ul>
-        </div>
-            <div className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8"}>
-                <h1>Search Page</h1>
-            </div>
-            <label>Price Range (£ per month): </label>
-            <input placeholder="e.g. 100-400"></input>
-            <label>Department: </label>
-            <input placeholder="e.g. Computing"></input>
-            <label>Year: </label>
-            <input placeholder="e.g. 2"></input>
-            <label>Night Owl</label>
-            <input type = "checkbox"></input>
-            <label>Morning Scrub</label>
-            <input type = "checkbox"></input>
-        </div>
-    )
+  )
 }
 /*
-    <label for="Price Range">Price Range:</label>
-    <input name="email" id="email" value=""></input>
-*/
+ <label for="Price Range">Price Range:</label>
+ <input name="email" id="email" value=""></input>
+ */
