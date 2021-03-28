a2d68e04905fea2db61148ebba4a682201843a13
shashwatkansal <sk4319@ic.ac.uk>
2021-03-28 10:17:00 +0100

    Add flathunt home page with linking

--- ic-hello-world/src/App.js
+++ ic-hello-world/src/App.js
@@ -1,55 +1,33 @@
 import React from 'react';
+import {useHistory} from "react-router";
 import './App.css';
-import {Counter} from './features/counter/Counter';
 import logo from './logo.png';
 
 function App() {
+  const history = useHistory();
+  const LinkButton = ({val, link}) => {
+    return (
+      <button
+        className={"bg-gradient-to-r from-blue-600 to-orange-600 text-white p-4 " +
+        "rounded-xl hover:scale-105 transform duration-150 font-bold focus:outline-none"}
+        onClick={() => history.push(`${link}`)}
+      >
+        {val}
+      </button>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
-        <Counter/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        {/*<Counter/>*/}
+        <h1
+          className={"text-4xl text-gray-900 text-center font-bold"}>FlatHunt</h1>
+        <div className={"grid grid-cols-3 p-8 gap-4 mt-8"}>
+          <LinkButton val={"Profile"} link={"/profile"}/>
+          <LinkButton val={"Housing"} link={"/housing"}/>
+          <LinkButton val={"Search"} link={"/search"}/>
+        </div>
       </header>
     </div>
   );
