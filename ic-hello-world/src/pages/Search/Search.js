e96391872254ddc1184ac5a3991880e3aeb84cb0
shashwatkansal <sk4319@ic.ac.uk>
2021-03-28 10:58:23 +0100

    Update Search with new ui

--- ic-hello-world/src/pages/Search/Search.js
+++ ic-hello-world/src/pages/Search/Search.js
@@ -23,26 +23,113 @@ export default function Search() {
           <li><Link to="/feed"> Feed</Link></li>
         </ul>
       </div>
-      <div
-        className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8"}>
-        <h1>Search Page</h1>
-      </div>
-      <label>Price Range (£ per month): </label>
-      <input placeholder="e.g. 100-400"></input>
-      <label>Department: </label>
-      <input placeholder="e.g. Computing"></input>
-      <label>Year: </label>
-      <input placeholder="e.g. 2"></input>
-      <label>Night Owl</label>
-      <input type="checkbox"></input>
-      <label>Morning Scrub</label>
-      <input type="checkbox"></input>
+      <SearchNew/>
       <div className={"mt-4 p-8"}>
         {/*{GetAllData()}*/}
       </div>
     </div>
   )
 }
+
+function SearchNew() {
+  return (
+    /*
+     <div className={"bg-gray-100 "}>
+     <div className={"text-center text-xl font-semibold py-8"}>
+     <h1 className={"font-bold text-4xl text-blue-600"}>Profile</h1>
+     <p>Fill out your profile so that you can connect with others!</p>
+     </div>
+     <div className={"bg-white rounded-xl shadow-xl mx-auto"}>
+     </div>
+     </div>
+     <div className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8 rounded-l-md border border-r-0 border-gray-300 bg-gray-50"}>
+     "col-span-6 sm:col-span-3 lg:col-span-2"
+     */
+    <div className={"bg-gray-100 rounded-md"}>
+      <div
+        className={"bg-gray-100 text-center text-4xl text-blue-600 font-bold py-8"}>
+        <h1>Search Page</h1>
+      </div>
+
+      <div className={"bg-white rounded-md"} position="relative" width="33%"
+           border="3px solid blue">
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label className={"block text-sm font-medium text-gray-700"}>Price
+              Range</label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="£ per month"
+                   type="text"></input>
+          </div>
+        </div>
+
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label
+              className={"block text-sm font-medium text-gray-700"}>Department</label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="e.g. Computing"
+                   type="text"></input>
+          </div>
+        </div>
+
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label
+              className={"block text-sm font-medium text-gray-700"}>Year</label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="e.g. 2"
+                   type="text"></input>
+          </div>
+        </div>
+
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label className={"block text-sm font-medium text-gray-700"}>Night
+              Owl? </label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="isNightOwl"
+                   type="checkbox"></input>
+          </div>
+        </div>
+
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label className={"block text-sm font-medium text-gray-700"}>Morning
+              Scrub? </label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="isMorningScrub"
+                   type="checkbox"></input>
+          </div>
+        </div>
+      </div>
+
+      <div left="50px" position="relative">
+        <div className={"col-span-6 sm:col-span-3 lg:col-span-2"}>
+          <div>
+            <label className={"block text-sm font-medium text-gray-700"}>Morning
+              Scrub? </label>
+          </div>
+          <div>
+            <input className={"rounded-md text-gray-500"} value="isMorningScrub"
+                   type="checkbox"></input>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/*
+ <label for="Price Range">Price Range:</label>
+ <input name="email" id="email" value=""></input>
+ */
+
 /*
  <label for="Price Range">Price Range:</label>
  <input name="email" id="email" value=""></input>
